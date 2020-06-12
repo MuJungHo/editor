95fccea3ca37da50b4d5fcf8ba03ba907155f6e6
Ron Ho <ande1218@gmail.com>
2020-06-12 22:41:48 +0800

    feat: Add normalize.css and poly fill

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import 'react-app-polyfill/ie11'
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import rootReducer from './reducers'
+import "./normalize.css"
+
 
 const store = createStore(rootReducer)
 
